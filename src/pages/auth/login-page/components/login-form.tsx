72e1920bd5bc427159cf050348857f1bc8daefba
LucasCavalheri <lucas.dev.carvalho@gmail.com>
2025-10-27 20:49:52 -0300

    register e login

--- src/pages/auth/login-page/components/login-form.tsx
+++ src/pages/auth/login-page/components/login-form.tsx
@@ -12,10 +12,56 @@ import {
   FieldSeparator
 } from '@/components/ui/field'
 import { Input } from '@/components/ui/input'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { loginSchema, type LoginSchema } from '@/schemas/auth/login-schema'
+import { isAxiosError } from 'axios'
+import { useAuth } from '@/contexts/auth-context'
+import { toast } from 'sonner'
+import { useGoogle } from '@/hooks/use-google'
+import { Loader2Icon } from 'lucide-react'
+import { cn } from '@/lib/utils'
 
 export function LoginForm() {
+  const { login } = useAuth()
+
+  const googleLogin = useGoogle()
+
+  const form = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  })
+
+  async function handleLogin(data: LoginSchema) {
+    try {
+      await login.execute(data)
+    } catch (error) {
+      if (isAxiosError(error)) {
+        if (error.status === 400) {
+          return toast.error('Erro ao fazer o login', {
+            description: error.response?.data.message
+          })
+        }
+
+        if (error.status === 401) {
+          return toast.error('Erro ao fazer o login', {
+            description: error.response?.data.message
+          })
+        }
+
+        return toast.error('Erro ao fazer o login', {
+          description:
+            'Houve um erro ao fazer o login. Por favor, tente novamente.'
+        })
+      }
+    }
+  }
+
   return (
-    <form className="p-6 md:p-8">
+    <form className="p-6 md:p-8" onSubmit={form.handleSubmit(handleLogin)}>
       <FieldGroup>
         <div className="flex flex-col items-center gap-2 text-center">
           <h1 className="text-2xl font-bold">Bem-vindo de volta</h1>
@@ -30,6 +76,11 @@ export function LoginForm() {
             type="email"
             placeholder="email@exemplo.com"
             required
+            className={cn(
+              'w-full',
+              form.formState.errors.email && 'border-destructive'
+            )}
+            {...form.register('email')}
           />
         </Field>
         <Field>
@@ -42,20 +93,43 @@ export function LoginForm() {
               Esqueceu sua senha?
             </Link>
           </div>
-          <div className="flex flex-col gap-1">
-            <Input
-              id="password"
-              type="password"
-              placeholder="*******"
-              required
-            />
-            <span className="text-xs text-muted-foreground">
-              Sua senha deve ter pelo menos 6 caracteres
-            </span>
+          <div>
+            <div className="flex flex-col gap-1">
+              <Input
+                id="password"
+                type="password"
+                placeholder="*******"
+                required
+                className={cn(
+                  'w-full',
+                  form.formState.errors.password && 'border-destructive'
+                )}
+                {...form.register('password')}
+              />
+              {!form.formState.errors.password && (
+                <FieldDescription>
+                  Sua senha deve ter pelo menos 6 caracteres
+                </FieldDescription>
+              )}
+            </div>
+            {form.formState.errors.password && (
+              <FieldDescription className="text-destructive">
+                {form.formState.errors.password.message}
+              </FieldDescription>
+            )}
           </div>
         </Field>
         <Field>
-          <Button type="submit">Login</Button>
+          <Button
+            type="submit"
+            disabled={form.formState.isSubmitting || login.isPending}
+          >
+            {form.formState.isSubmitting || login.isPending ? (
+              <Loader2Icon className="mr-2 h-4 w-4 animate-spin" />
+            ) : (
+              'Login'
+            )}
+          </Button>
         </Field>
         <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
           Ou continue com
@@ -64,7 +138,12 @@ export function LoginForm() {
           <Button variant="outline" type="button">
             <AppleIconSvg />
           </Button>
-          <Button variant="outline" type="button">
+          <Button
+            variant="outline"
+            type="button"
+            onClick={() => googleLogin.execute()}
+            disabled={googleLogin.isPending}
+          >
             <GoogleIconSvg />
           </Button>
           <Button variant="outline" type="button">
--- src/pages/auth/register-page/components/register-form.tsx
+++ src/pages/auth/register-page/components/register-form.tsx
@@ -12,15 +12,64 @@ import {
   FieldSeparator
 } from '@/components/ui/field'
 import { Input } from '@/components/ui/input'
+import { useAuth } from '@/contexts/auth-context'
+import { useGoogle } from '@/hooks/use-google'
+import {
+  registerSchema,
+  type RegisterSchema
+} from '@/schemas/auth/register-schema'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { isAxiosError } from 'axios'
+import { toast } from 'sonner'
+import { Loader2Icon } from 'lucide-react'
+import { cn } from '@/lib/utils'
 
 export function RegisterForm() {
+  const { register } = useAuth()
+
+  const googleRegister = useGoogle()
+
+  const form = useForm<RegisterSchema>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: ''
+    }
+  })
+
+  async function handleRegister(data: RegisterSchema) {
+    try {
+      await register.execute(data)
+    } catch (error) {
+      if (isAxiosError(error)) {
+        if (error.status === 409) {
+          form.setError('email', {
+            type: 'manual',
+            message: error.response?.data.message
+          })
+
+          return toast.error('Erro ao fazer o cadastro', {
+            description: error.response?.data.message
+          })
+        }
+
+        return toast.error('Erro ao fazer o cadastro', {
+          description:
+            'Houve um erro ao fazer o cadastro. Por favor, tente novamente.'
+        })
+      }
+    }
+  }
+
   return (
-    <form className="p-6 md:p-8">
+    <form className="p-6 md:p-8" onSubmit={form.handleSubmit(handleRegister)}>
       <FieldGroup>
         <div className="flex flex-col items-center gap-2 text-center">
           <h1 className="text-2xl font-bold">Cadastro</h1>
           <p className="text-muted-foreground text-balance">
-            Faça cadastro em sua conta {env.VITE_APP_NAME}!
+            Faça cadastro na plataforma {env.VITE_APP_NAME}!
           </p>
         </div>
         <Field>
@@ -30,7 +79,17 @@ export function RegisterForm() {
             type="text"
             placeholder="Seu nome completo"
             required
+            className={cn(
+              'w-full',
+              form.formState.errors.name && 'border-destructive'
+            )}
+            {...form.register('name')}
           />
+          {form.formState.errors.name && (
+            <FieldDescription className="text-destructive">
+              {form.formState.errors.name.message}
+            </FieldDescription>
+          )}
         </Field>
         <Field>
           <FieldLabel htmlFor="email">Email</FieldLabel>
@@ -39,7 +98,17 @@ export function RegisterForm() {
             type="email"
             placeholder="email@exemplo.com"
             required
+            className={cn(
+              'w-full',
+              form.formState.errors.email && 'border-destructive'
+            )}
+            {...form.register('email')}
           />
+          {form.formState.errors.email && (
+            <FieldDescription className="text-destructive">
+              {form.formState.errors.email.message}
+            </FieldDescription>
+          )}
         </Field>
         <Field>
           <div className="flex items-center">
@@ -51,20 +120,43 @@ export function RegisterForm() {
               Esqueceu sua senha?
             </Link>
           </div>
-          <div className="flex flex-col gap-1">
-            <Input
-              id="password"
-              type="password"
-              placeholder="*******"
-              required
-            />
-            <span className="text-xs text-muted-foreground">
-              Sua senha deve ter pelo menos 6 caracteres
-            </span>
+          <div>
+            <div className="flex flex-col gap-1">
+              <Input
+                id="password"
+                type="password"
+                placeholder="*******"
+                required
+                className={cn(
+                  'w-full',
+                  form.formState.errors.password && 'border-destructive'
+                )}
+                {...form.register('password')}
+              />
+              {!form.formState.errors.password && (
+                <FieldDescription>
+                  Sua senha deve ter pelo menos 6 caracteres
+                </FieldDescription>
+              )}
+            </div>
+            {form.formState.errors.password && (
+              <FieldDescription className="text-destructive">
+                {form.formState.errors.password.message}
+              </FieldDescription>
+            )}
           </div>
         </Field>
         <Field>
-          <Button type="submit">Cadastrar</Button>
+          <Button
+            type="submit"
+            disabled={form.formState.isSubmitting || register.isPending}
+          >
+            {form.formState.isSubmitting || register.isPending ? (
+              <Loader2Icon className="mr-2 h-4 w-4 animate-spin" />
+            ) : (
+              'Cadastrar'
+            )}
+          </Button>
         </Field>
         <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
           Ou continue com
@@ -73,7 +165,12 @@ export function RegisterForm() {
           <Button variant="outline" type="button">
             <AppleIconSvg />
           </Button>
-          <Button variant="outline" type="button">
+          <Button
+            variant="outline"
+            type="button"
+            disabled={googleRegister.isPending}
+            onClick={() => googleRegister.execute()}
+          >
             <GoogleIconSvg />
           </Button>
           <Button variant="outline" type="button">
